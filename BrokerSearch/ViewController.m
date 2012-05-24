5b25e632dbcc4517d9bb6628e13c0dc7b15f7132
Mark Houghton <mark1284@gmail.com>
2012-05-24 17:10:17 +0100

    a working'ish version

--- BrokerSearch/ViewController.m
+++ BrokerSearch/ViewController.m
@@ -5,18 +5,35 @@
 //  Created by Mark Houghton on 23/05/2012.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+// see http://www.raywenderlich.com/5492/working-with-json-in-ios-5 for JSON stuff and a touch of GCD
+// see http://ygamretuta.me/2011/08/10/ios-implementing-a-basic-search-uisearchdisplaycontroller-and-interface-builder/ for UISearchControllerStuff
+//
+//
+// Connected to LT020671 running customised version of OppTracker:-
+// - add hosts entry;
+// - disable session expired filter in OppTracker
+// - removed security from searchBrokerAsJSON method (otherwise we'd need to be logged in)
+//
+//  To disable DEBUG, search for 'macros' in the project build settings and delete the DEBUG macro definition
+//
+// Improvements
+// - implement scope filtering (OppTracker change required)
+// - implement bookmarks
+// - use broker objects rather than strings
+// - use GCD to move data retrieval from main thread
+// - unit tests
+// - start writing a bespoke Opptracker service: OppTracker-iOS
+// - externalize URL
+//
 
 #import "ViewController.h"
 
-//BACKGROUND QUEUE FROM GCD
+
+#define searchBrokerURL @"http://lt020671:8080/OppTracker/brokerMain/searchBrokerAsJSON?includeAquila=true&query="
+
+
+//MAYBE USE GCD BACKGROUND QUEUE FROM
 #define kBgQueue dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-
-//URL FOR DATA
-#define kLatestKivaLoansURL [NSURL URLWithString: @"http://api.kivaws.org/v1/loans/search.json?status=fundraising"]
-
-//needs to be mutable
-//#define searchBrokerAsJSONURL [NSURL URLWithString: @"http://appd041a:8080/OppTracker/brokerMain/searchBrokerAsJSON"]
-
 
 
 @interface ViewController ()
@@ -36,37 +53,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    
-    // [self.tableView reloadData];
     self.tableView.scrollEnabled = YES;
     
-    //TODO  REPLACE WITH JSON LOOKUP
-//    NSArray *items = [[NSArray alloc] initWithObjects:
-//                      @"Alexander Forbes",
-//                      @"Aon Hewitt'",
-//                      @"Atlas consulting",
-//                      @"AWD Chase",
-//                      @"Barclays",
-//                      @"Bartlett Group",
-//                      nil];
-    
-    
-    // start the data download ona background thread queue managed by GCD
-  //  dispatch_async(kBgQueue, ^{
-        //NSData* data = [NSData dataWithContentsOfURL:kLatestKivaLoansURL];
-        
-        // when finished execute the fetched data on the main thread
-       // [self performSelectorOnMainThread:@selector(fetchedData:) withObject:data waitUntilDone:YES];
-    //});
-    
-    //[self fetchedData:data];
-    
-    //self.allItems = items;
-    
-    //[self.tableView reloadData];
-    
+    // populate the base list with the top 10: could change to last used maybe 
     [self filterContentForSearchTextFromOppTracker : @""];
+    [self.tableView reloadData];
+    
 }
 
 - (void)viewDidUnload
@@ -74,7 +66,6 @@
     [self setSearchDisplayController:nil];
     [self setSearchBar:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -87,6 +78,7 @@
 }
 
 
+// remember this controller is handling two views: the base view and the search results
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     NSInteger rows = 0;
@@ -102,7 +94,7 @@
 }
 
 
-// Customize the appearance of table view cells.
+// remember this controller is handling two views: the base view and the search results
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
@@ -111,17 +103,23 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
         // this bit adds the > at the end of each table row
-        //cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
     /* Configure the cell. */
     if ([tableView isEqual:self.searchDisplayController.searchResultsTableView]){
         cell.textLabel.text = 
         [self.searchResults objectAtIndex:indexPath.row];
+        #ifdef DEBUG
+            NSLog(@"SearchResult %@", [self.searchResults objectAtIndex:indexPath.row]);
+        #endif
     }
     else{
         cell.textLabel.text =
         [self.allItems objectAtIndex:indexPath.row];
+        #ifdef DEBUG
+            NSLog(@"AllResult %@", [self.allItems objectAtIndex:indexPath.row]);
+        #endif
     }
     
     return cell;
@@ -132,120 +130,99 @@
 
 -(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
 {
-    // recall the JSON service rather than filter the array list
+    // recall the JSON service
+    [self filterContentForSearchTextFromOppTracker : searchString];
     
-    [self filterContentForSearchText:searchString 
-                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
-                                      objectAtIndex:[self.searchDisplayController.searchBar
-                                                     selectedScopeButtonIndex]]];
-    
+    // technically there is only one broker list...but lets retain the base list / search list metaphore
+    self.searchResults = allItems; 
     return YES;
 }
 
+
+
+
+#pragma mark - 
+// scope is the scope of the search e.g all brokers or just my brokers
 - (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
 {
-   
     // recall the JSON service
-    
-    [self filterContentForSearchText:[self.searchDisplayController.searchBar text] 
-                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
-                                      objectAtIndex:searchOption]];
-    
-    return YES;
+    return NO;
 }
 
-#pragma FILTER THE SEARCH RESULTS
-- (void)filterContentForSearchText:(NSString*)searchText 
-                             scope:(NSString*)scope
-{
-    NSPredicate *resultPredicate = [NSPredicate 
-                                    predicateWithFormat:@"SELF contains[cd] %@",
-                                    searchText];
-    
-    self.searchResults = [self.allItems filteredArrayUsingPredicate:resultPredicate];
-}
+#pragma mark FILTER THE SEARCH RESULTS FROM OPPTRACKER
+- (void)filterContentForSearchTextFromOppTracker:(NSString*)searchText 
 
-
-
-
-
-
-- (void) fetchedData: (NSData *) responseData 
 {
     
+    // needed to add hosts entry, disable session expired filter and remove security from JSON method to get this working
+    NSMutableString* urlStr =  [NSMutableString new] ;
+    [urlStr setString :  searchBrokerURL];
     
-    // PARSE THE RESPONSE DATA
-    NSError* error;
-    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
-    NSArray* latestLoans = [json objectForKey:@"loans"];
-//    NSLog(@"loans: %@",latestLoans);
+    NSString* escapedSearchText =[searchText stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding];
+    #ifdef DEBUG
+        NSLog(@"escaped search string is:%@",escapedSearchText);
+    #endif
+    [urlStr appendString:escapedSearchText];
+    #ifdef DEBUG
+        NSLog(@"url is:%@",urlStr);
+    #endif
+    NSURL* url = [NSURL URLWithString:urlStr];
+    NSError* error = nil;   
+    NSData* data = [NSData dataWithContentsOfURL: url  options: NSDataReadingUncached error: &error];
     
+    #ifdef DEBUG
+        NSLog(@"error is:%@",error);
+        NSLog(@"data is:%@",data);
+    #endif
     
-    
-    
-    // iterate over each NSDictionary type
-    NSMutableArray* activities = [NSMutableArray new];
-    for (id loan in latestLoans) {
-        [activities addObject:[loan objectForKey:@"activity"]];        
+    if (data != nil) {
+        [self fetchedDataForOppTracker:data]; // this could be async
     }
     
-    allItems = activities;
-    
-    
-    
-    
-//    NSDictionary* loan = [latestLoans objectAtIndex:0];
-//    
-//    NSNumber* fundedAmount = [loan objectForKey:@"funded_amount"];
-//    NSNumber* loanAmount = [loan objectForKey:@"loan_amount"];
-//    
-//    float outstandingAmount = [loanAmount floatValue] - [fundedAmount floatValue];
-//    
-//    humanReadable.text = [NSString stringWithFormat:@"Latest loan: %@ from %@ needs another $%.2f to pursue their entreprenueal dream", 
-//                          [loan objectForKey:@"name"],
-//                          [(NSDictionary*)[loan objectForKey:@"location"] 
-//                           objectForKey:@"country"],
-//                          outstandingAmount];
-//    
-//    
-//    
-//    //build an info object and convert to json
-//    NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
-//                          [loan objectForKey:@"name"], @"who",
-//                          [(NSDictionary*)[loan objectForKey:@"location"] 
-//                           objectForKey:@"country"], @"where",
-//                          [NSNumber numberWithFloat: outstandingAmount], @"what",nil];
-//    
-//    
-//    // CREATE A JSON RESPONSE  ,
-//    
-//    //convert object to data
-//    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:info 
-//                                                       options:NSJSONWritingPrettyPrinted error:&error];
-//    
-//    //print out the data contents
-//    jsonSummary.text = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     
 }
 
 
-#pragma FILTER THE SEARCH RESULTS FROM OPPTRACKER
-- (void)filterContentForSearchTextFromOppTracker:(NSString*)searchText 
-                          
+#pragma mark use real broker objects!
+
+-(void) fetchedDataForOppTracker: (NSData*) responseData
 {
- 
-    // append the query string to the url
-    NSMutableString* url =[NSMutableString stringWithCapacity: 1000];
-                           
-    [url appendString: @"http://appd041a:8080/OppTracker/brokerMain/searchBrokerAsJSON?query="] ;
-    
-    NSData* data = [NSData dataWithContentsOfURL: [NSURL URLWithString: url]];
-    
-    [self fetchedData:data]; // this could be async
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+    NSArray* result = [json objectForKey:@"result"];
+    #ifdef DEBUG
+        NSLog(@"result is %@",result);
+    #endif 
+    NSMutableArray* brokers = [NSMutableArray new];
+    for(id entry in result) {
+        NSNumber* brokerId = [entry objectForKey:@"id"];
+        NSString* brokerName = [entry objectForKey:@"name"];
+        #ifdef DEBUG
+            NSLog(@"id is %@",brokerId);
+            NSLog(@"name is %@",brokerName);
+        #endif        
+        [brokers addObject: brokerName];        
+    }
+    allItems = brokers;
+}
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSUInteger row = indexPath.row;
+    NSString *rowValue;
     
+    if ([tableView isEqual:self.searchDisplayController.searchResultsTableView]){
+        rowValue = [searchResults objectAtIndex:row];
+    } else {
+        rowValue = [allItems objectAtIndex:row];
+    }
+    NSString *message = [[NSString alloc] initWithFormat:@"You selected %@", rowValue];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Broker Selected!" message:message delegate:nil cancelButtonTitle: @"Ok" otherButtonTitles:nil, nil];
+    [alert show];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
 }
+
 
 
 @end
