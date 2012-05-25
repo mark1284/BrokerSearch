dc5a273072a191858e40499b7ac5871790c2edd1
Mark Houghton <mark1284@gmail.com>
2012-05-25 11:44:45 +0100

    indenting. added url to plist. clarified sarchresults

--- BrokerSearch/ViewController.m
+++ BrokerSearch/ViewController.m
@@ -22,18 +22,14 @@
 // - use broker objects rather than strings
 // - use GCD to move data retrieval from main thread
 // - unit tests
-// - start writing a bespoke Opptracker service: OppTracker-iOS
-// - externalize URL
+// - use a bespoke Opptracker service: OppTracker-iOS
+// 
 //
 
 #import "ViewController.h"
 
-
-#define searchBrokerURL @"http://lt020671:8080/OppTracker/brokerMain/searchBrokerAsJSON?includeAquila=true&query="
-
-
 //MAYBE USE GCD BACKGROUND QUEUE FROM
-#define kBgQueue dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+//#define kBgQueue dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 
 
 @interface ViewController ()
@@ -49,13 +45,23 @@
 @synthesize allItems;
 @synthesize searchResults;
 
+NSString* oppTrackerUrl;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+//    read the url from the plist
+    NSBundle* mainBundle;
+    mainBundle = [NSBundle mainBundle];
+    oppTrackerUrl = [mainBundle objectForInfoDictionaryKey:@"OppTrackerUrl"];    
+#ifdef DEBUG
+    NSLog(@"url from plit is= %@",  oppTrackerUrl);
+#endif
+    
     self.tableView.scrollEnabled = YES;
     
-    // populate the base list with the top 10: could change to last used maybe 
+//     populate the base list with the top 10: could change to last used maybe 
     [self filterContentForSearchTextFromOppTracker : @""];
     [self.tableView reloadData];
     
@@ -79,6 +85,7 @@
 
 
 // remember this controller is handling two views: the base view and the search results
+// return the number of rows from the appropriate list
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     NSInteger rows = 0;
@@ -86,7 +93,7 @@
     if ([tableView isEqual:self.searchDisplayController.searchResultsTableView]){
         rows = [self.searchResults count];
     }
-    else{
+    else {
         rows = [self.allItems count];
     }
     
@@ -102,26 +109,24 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        // this bit adds the > at the end of each table row
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+//         this bit adds the > at the end of each table row
+//         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
     /* Configure the cell. */
     if ([tableView isEqual:self.searchDisplayController.searchResultsTableView]){
         cell.textLabel.text = 
         [self.searchResults objectAtIndex:indexPath.row];
-        #ifdef DEBUG
-            NSLog(@"SearchResult %@", [self.searchResults objectAtIndex:indexPath.row]);
-        #endif
+        
     }
     else{
-        cell.textLabel.text =
+        cell.textLabel.text = 
         [self.allItems objectAtIndex:indexPath.row];
-        #ifdef DEBUG
-            NSLog(@"AllResult %@", [self.allItems objectAtIndex:indexPath.row]);
-        #endif
+        
     }
-    
+#ifdef DEBUG
+    NSLog(@"cell text label= %@",  cell.textLabel.text);
+#endif
     return cell;
 }
 
@@ -130,14 +135,13 @@
 
 -(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
 {
-    // recall the JSON service
+//     recall the JSON service
     [self filterContentForSearchTextFromOppTracker : searchString];
     
-    // technically there is only one broker list...but lets retain the base list / search list metaphore
+//     technically there is only one broker list...but lets retain the base list / search list metaphore
     self.searchResults = allItems; 
     return YES;
 }
-
 
 
 
@@ -145,7 +149,7 @@
 // scope is the scope of the search e.g all brokers or just my brokers
 - (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
 {
-    // recall the JSON service
+// recall the JSON service
     return NO;
 }
 
@@ -154,26 +158,26 @@
 
 {
     
-    // needed to add hosts entry, disable session expired filter and remove security from JSON method to get this working
+// needed to add hosts entry, disable session expired filter and remove security from JSON method to get this working
     NSMutableString* urlStr =  [NSMutableString new] ;
-    [urlStr setString :  searchBrokerURL];
+    [urlStr setString :  oppTrackerUrl];
     
     NSString* escapedSearchText =[searchText stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding];
-    #ifdef DEBUG
-        NSLog(@"escaped search string is:%@",escapedSearchText);
-    #endif
+#ifdef DEBUG
+    NSLog(@"escaped search string is:%@",escapedSearchText);
+#endif
     [urlStr appendString:escapedSearchText];
-    #ifdef DEBUG
-        NSLog(@"url is:%@",urlStr);
-    #endif
+#ifdef DEBUG
+    NSLog(@"url is:%@",urlStr);
+#endif
     NSURL* url = [NSURL URLWithString:urlStr];
     NSError* error = nil;   
     NSData* data = [NSData dataWithContentsOfURL: url  options: NSDataReadingUncached error: &error];
     
-    #ifdef DEBUG
-        NSLog(@"error is:%@",error);
-        NSLog(@"data is:%@",data);
-    #endif
+#ifdef DEBUG
+    NSLog(@"error is:%@",error);
+    NSLog(@"data is:%@",data);
+#endif
     
     if (data != nil) {
         [self fetchedDataForOppTracker:data]; // this could be async
@@ -183,24 +187,24 @@
 }
 
 
-#pragma mark use real broker objects!
+#pragma mark TODO use real broker objects!
 
 -(void) fetchedDataForOppTracker: (NSData*) responseData
 {
     NSError* error;
     NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
     NSArray* result = [json objectForKey:@"result"];
-    #ifdef DEBUG
-        NSLog(@"result is %@",result);
-    #endif 
+#ifdef DEBUG
+    NSLog(@"result is %@",result);
+#endif 
     NSMutableArray* brokers = [NSMutableArray new];
     for(id entry in result) {
         NSNumber* brokerId = [entry objectForKey:@"id"];
         NSString* brokerName = [entry objectForKey:@"name"];
-        #ifdef DEBUG
-            NSLog(@"id is %@",brokerId);
-            NSLog(@"name is %@",brokerName);
-        #endif        
+#ifdef DEBUG
+        NSLog(@"id is %@",brokerId);
+        NSLog(@"name is %@",brokerName);
+#endif        
         [brokers addObject: brokerName];        
     }
     allItems = brokers;
